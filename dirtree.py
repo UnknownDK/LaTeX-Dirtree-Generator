9d636d6e84ee5f07a96fe5af1d99665ada2b9c00
Bjørn Grevenkop-Castenskiold <bhgreven@pm.me>
2023-05-20 18:41:01 +0200

    Back to the working version

--- dirtree.py
+++ dirtree.py
@@ -4,48 +4,37 @@ import os
 # \usepackage{dirtree}
 
 # E.g. C:/Users/user/Desktop/Folder/ on windows or /home/user/Desktop/Folder/ on linux.
-dir = '/home/greven/Projects/LaTeX-Dirtree-Generator/'
+dir = '/home/greven/Desktop/LarsVRKlo/'
 
 # Link to the Github e.g. https://github.com/UnknownDK/LaTeX-Dirtree-Generator/
-gitlink = "https://github.com/UnknownDK/LaTeX-Dirtree-Generator/"
+gitlink = "https://github.com/simo389b/LarsVRKlo/"
 
 # Name of branch
-branch = "main"
+branch = "master"
 
 # Ignore list. This can be specific files/folders or even file extensions e.g. script.py, secretfolder, .git.
-ignore = [".git"]
-# Whitelist. This will triumph over ignore. So if a folder e.g. /Data/ is ignored, but .txt is whitelisted,
-#  .txts will be included, but the rest of the folder will be ignored.
-whitelist = []
+ignore = [".git", ".mat", "Image", "ISM", "Reverb", "AllpassFilter_class", "HeadphoneFilter", "FDN", "Absorption", ".pdf", ".png", ".txt", "Data", "Responses", "roomGeneratorSpas", "Parameters"]
 
 
 def list_files(startpath):
     print("\dirtree{%")
-    alreadyPrinted = []
     for root, dirs, files in os.walk(startpath):
-        dirPrinted = False
-        level = root.replace(startpath, '').count(os.sep) + 1
-        if os.path.basename(root) != "":
-            level += 1
-        folders = root.replace('\\', '/').split('/')
-        for fold in reversed(folders):
-            if fold in dir:
-                del(folders[folders.index(fold)])
-
-        folders.append("")
-        folderpath = ""
-        for folder in folders:
-            if folder != "":
+        if not any(ele in root for ele in ignore):
+            level = root.replace(startpath, '').count(os.sep) + 1
+            if os.path.basename(root) != "":
+                level += 1
+            folders = root.replace('\\', '/').split('/')
+            for fold in reversed(folders):
+                if fold in dir:
+                    del(folders[folders.index(fold)])
+            folderpath = ""
+            for folder in folders:
                 folderpath += folder + "/"
+            print('.' + str(level) + ' \href{' + gitlink + 'tree/master/' + folderpath + '}{' + os.path.basename(root).replace("_", "\_") + '/}.' )
             for f in files:
-                if not any(ele in (folderpath + f) for ele in ignore) or any(ele in (folderpath + f) for ele in whitelist):
-                    if dirPrinted == False:
-                        print('.' + str(level) + ' \href{' + gitlink + 'tree/' + branch + '/' + folderpath + '}{' + os.path.basename(root).replace("_", "\_") + '/}.' )
-                        dirPrinted = True
-                    if f not in alreadyPrinted:
-                        filename = f.replace("_", "\_")
-                        print('.' + str(level + 1) + ' \href{' + gitlink + 'blob/' + branch + '/' + folderpath + f + '}{' + filename + '}.' )
-                        alreadyPrinted.append(f)
+                if not any(ele in f for ele in ignore):
+                    filename = f.replace("_", "\_")
+                    print('.' + str(level + 1) + ' \href{' + gitlink + 'blob/master/' + folderpath + f + '}{' + filename + '}.' )
     print("}")
 
 list_files(dir)
