4b16e72bca2e50a20e51181d9a77b1d48fa59a66
Bjørn Grevenkop-Castenskiold <bhgreven@pm.me>
2024-05-14 09:36:56 +0200

    Auto-add gitignore entries to ignore list

--- dirtree.py
+++ dirtree.py
@@ -4,22 +4,32 @@ import os
 # \usepackage{dirtree}
 
 # E.g. C:/Users/user/Desktop/Folder/ on windows or /home/user/Desktop/Folder/ on linux.
-dir = '/home/greven/Desktop/LarsVRKlo/'
+dir = '/home/greven/repos/ES10-Drowning/'
 
 # Link to the Github e.g. https://github.com/UnknownDK/LaTeX-Dirtree-Generator/
-gitlink = "https://github.com/simo389b/LarsVRKlo/"
+gitlink = "https://github.com/UnknownDK/ES10-Drowning/"
 
 # Name of branch
 branch = "master"
 
 # Ignore list. This can be specific files/folders or even file extensions e.g. script.py, secretfolder, .git.
-ignore = [".git", ".mat", "Image", "ISM", "Reverb", "AllpassFilter_class", "HeadphoneFilter", "FDN", "Absorption", ".pdf", ".png", ".txt", "Data", "Responses", "roomGeneratorSpas", "Parameters"]
+# Everything in every .gitignore is automatically added
+ignore = [".git",]
 
 
 def list_files(startpath):
+    ignore_list = []
+    # Walk the directory tree to find .gitignore files and extract their rules
+    for root, dirs, files in os.walk(startpath):
+        if '.gitignore' in files:
+            with open(os.path.join(root, '.gitignore'), 'r') as ignore_file:
+                ignore_list.extend(
+                    [os.path.join(root, line.strip()) for line in ignore_file.readlines() if not line.startswith('#')]
+                )
+
     print("\dirtree{%")
     for root, dirs, files in os.walk(startpath):
-        if not any(ele in root for ele in ignore):
+        if not any(ele in root for ele in ignore) and not any(ele in root for ele in ignore_list):
             level = root.replace(startpath, '').count(os.sep) + 1
             if os.path.basename(root) != "":
                 level += 1
@@ -32,7 +42,7 @@ def list_files(startpath):
                 folderpath += folder + "/"
             print('.' + str(level) + ' \href{' + gitlink + 'tree/master/' + folderpath + '}{' + os.path.basename(root).replace("_", "\_") + '/}.' )
             for f in files:
-                if not any(ele in f for ele in ignore):
+                if not any(ele in f for ele in ignore) and not any(ele in os.path.join(root, f) for ele in ignore_list):
                     filename = f.replace("_", "\_")
                     print('.' + str(level + 1) + ' \href{' + gitlink + 'blob/master/' + folderpath + f + '}{' + filename + '}.' )
     print("}")
